5352df133fad47debe736c7306a298ec715592b9
Cesar <cesarfranco32@gmail.com>
2024-09-20 16:55:08 -0600

    Paginacion

--- app/order/layout.tsx
+++ app/order/layout.tsx
@@ -1,9 +1,12 @@
 import OrderSidebar from "@/components/order/OrderSidebar";
 import OrderSummary from "@/components/order/OrderSummary";
+import ToastNotification from "@/components/ui/ToastNotification";
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
 
     return(
+        <>
+        
         <div className="md:flex">
             <OrderSidebar/>
             <main className="md:flex-1 md:h-screen md:overflow-y-scroll p-5">
@@ -11,6 +14,10 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
             </main>
             <OrderSummary/>
         </div>
+
+        <ToastNotification/>
+        
+        </>
     )
 
 }
---
+++ components/products/AddProductButon.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useStore } from "@/src/store"
+import { Product } from "@prisma/client"
+
+
+type AddProductButonProps = {
+    product: Product
+}
+
+export default function AddProductButon({product}: AddProductButonProps) {
+
+    const addToOrder = useStore((state) => state.addToOrder )
+
+  return (
+    <button 
+            type="button"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+            onClick={() => addToOrder(product)}
+            >Agregar</button>
+  )
+}
