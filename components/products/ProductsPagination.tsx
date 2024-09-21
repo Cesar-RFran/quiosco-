25e5130cf219040eca8dca2b577fb6f75c352f70
Cesar <cesarfranco32@gmail.com>
2024-09-21 00:06:04 -0600

    Seccion 65 terminada

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -6,6 +6,7 @@ type ProductsPaginationProps = {
 }
 
 export default function ProductsPagination({page, totalPages}: ProductsPaginationProps) {
+   const pages =  Array.from({length: totalPages}, (_, i ) => i+ 1)
   return (
     <nav className="flex justify-center py-10">
             {page > 1 && (
@@ -14,6 +15,17 @@ export default function ProductsPagination({page, totalPages}: ProductsPaginatio
                 href={`/admin/products?page=${page - 1}`}
                 >&laquo;</Link>
             )}
+
+            {pages.map(currentPage => (
+              <Link
+              key={currentPage}
+              href={`/admin/products?page=${currentPage}`}
+              className={ `${page === currentPage ? 'font-black bg-amber-400' : 'bg-white'}  px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-9`}
+              >
+              {currentPage}
+
+              </Link>
+            ))}
             
 
         {page < totalPages && (
