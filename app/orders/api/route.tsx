04927ea61bfc551cf6ff8ebf47d994c89d218722
Cesar <cesarfranco32@gmail.com>
2024-09-22 18:49:29 -0600

    68 fin

---
+++ app/orders/api/route.tsx
@@ -0,0 +1,23 @@
+import { prisma } from "@/src/lib/prisma";
+
+export async function GET() {
+    const orders = await prisma.order.findMany({
+        take: 5,
+        where: {
+            orderReadyAr: {
+                not: null
+            }
+        },
+        orderBy: {
+            orderReadyAr: 'desc'
+        },
+        include: {
+            orderProducts: {
+                include: {
+                    product: true
+                }
+            }
+        }
+    })
+    return Response.json(orders)
+}
